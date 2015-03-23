77aff55921cd8dadaa0724751630fd0dd1cd2d98
Nathan Bouscal <nbouscal@gmail.com>
2015-03-22 23:10:27 -0700

    send POST / to units#convert

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  post '' => 'units#convert'
   post 'units/convert/' => 'units#convert'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
